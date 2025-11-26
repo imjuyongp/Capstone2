a73bd6ff5ffa96fe63b53c27bf7a9a99469d107c
imjuyongp <imjuyongp@gmail.com>
2025-11-26 13:25:03 +0900

    :sparkles:Feat: 혼잡도 레이블 정의

--- traffic-service/src/main/java/com/project/trafficservice/domain/traffic/entity/Congestion.java
+++ traffic-service/src/main/java/com/project/trafficservice/domain/traffic/entity/Congestion.java
@@ -1,4 +1,16 @@
 package com.project.trafficservice.domain.traffic.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Congestion {
+    EMPTY("거의 없음"),
+    LOW("여유"),
+    MIDDLE("보통"),
+    HIGH("혼집"),
+    FULL("매우 혼잡");
+
+    private final String description;
 }
